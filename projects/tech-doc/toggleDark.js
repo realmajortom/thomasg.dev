ebfae3dafd441d839d8819754c6c56b7b8db74b5
Thomas Girgenti <thomasg@disroot.org>
2019-04-30 01:14:09 -0600

    add dark mode for tech docs

---
+++ projects/tech-doc/toggleDark.js
@@ -0,0 +1,15 @@
+function toggleDark() {
+  const body = document.getElementById('big-body');
+  const codeBlocks = document.getElementsByClassName('code-container');
+  if (body.classList.contains('body-dark')) {
+    body.classList.remove('body-dark');
+    for (let i = 0; i < codeBlocks.length; i++) {
+      codeBlocks[i].classList.remove('code-dark');
+    }
+  } else {
+    body.classList.add('body-dark');
+    for (let i = 0; i < codeBlocks.length; i++) {
+      codeBlocks[i].classList.add('code-dark');
+    }
+  }
+}
