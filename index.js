e235cda7881b3a438b28c3148c22bb3cf30d907d
Thomas Girgenti <thomasg@disroot.org>
2019-05-14 00:52:02 -0600

    improve filenam conventions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const navLink = document.getElementsByClassName('barlink');
 const classPairs = [
   ['header-container', 'header-pin'],
@@ -39,3 +40,15 @@ setInterval(function () {
   }
 }, 100);
 
+
+const hamburger = document.querySelector('.hamburger');
+const mobileLink = document.getElementsByClassName('mobile-link');
+const mobileNav = document.getElementById('navbar-mobile');
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('is-active');
+  mobileNav.classList.toggle('height');
+  for (let l = 0; l < mobileLink.length; l++) {
+    mobileLink[l].classList.toggle('display-none');
+  }
+});
--- js/hamburger.js
+++ js/hamburger.js
@@ -1,8 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
 const mobileLink = document.getElementsByClassName('mobile-link');
+const mobileNav = document.getElementById('navbar-mobile');
 
 hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('is-active');
+  mobileNav.classList.toggle('height');
   for (let l = 0; l < mobileLink.length; l++) {
     mobileLink[l].classList.toggle('display-none');
   }
