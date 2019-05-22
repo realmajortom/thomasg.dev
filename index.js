66ab5d758cae95cb609dad32d9ca1e29b15b7849
Thomas Girgenti <thomasg@disroot.org>
2019-05-21 21:03:50 -0600

    major touch ups..

--- index.js
+++ index.js
@@ -1,63 +1,42 @@
-
-const navLink = document.getElementsByClassName('barlink');
-const classPairs = [
-  ['header-container', 'header-pin'],
-  ['header-text', 'header-text-pin'],
-  ['name-text', 'name-text-pin'],
-  ['job-text', 'display-none'],
-  ['scroll-link', 'display-none'],
-  ['icon-container', 'icon-cont-pin'],
-  ['navbar-desktop', 'navbar-pin'],
-  ['ham-menu', 'show-ham'],
-  ['body', 'scroll-on']
-];
-
-window.onscroll = function () {
-  adjustHeader();
-};
-function adjustHeader() {
-  if (window.pageYOffset >= 1) {
-    for (let p = 0; p < classPairs.length; p++) {
-      document.getElementById(classPairs[p][0]).classList.add(classPairs[p][1]);
-    }
-    for (let n = 0; n < navLink.length; n++) {
-      navLink[n].classList.add('barlink-pin');
-    }
-  }
-  /*
-  else {
-    for (let p = 0; p < classPairs.length; p++) {
-      document.getElementById(classPairs[p][0]).classList.remove(classPairs[p][1]);
-    }
-    for (let n = 0; n < navLink.length; n++) {
-      navLink[n].classList.remove('barlink-pin');
-    }
-  }
-  */
-}
-
-const hamburger = document.querySelector('.hamburger');
-const mobileLink = document.getElementsByClassName('mobile-link');
-const mobileNav = document.getElementById('navbar-mobile');
-
-hamburger.addEventListener('click', function () {
-  hamburger.classList.toggle('is-active');
-  mobileNav.classList.toggle('height');
-  for (let l = 0; l < mobileLink.length; l++) {
-    mobileLink[l].classList.toggle('display-none');
-  }
-});
-
-
 $(document).ready(function () {
-  $('#name-text').click(function () {
-    for (let p = 0; p < classPairs.length; p++) {
-      document.getElementById(classPairs[p][0]).classList.remove(classPairs[p][1]);
+
+  const shrinkThings = [
+    ['#headerCont', 'headerCont-shrink'],
+    ['#headerMain', 'headerMain-shrink'],
+    ['#headerTitle', 'headerTitle-shrink'],
+    ['#body', 'scroll-on'],
+    ['#continueBtn', 'noShow'],
+    ['#headerSub', 'noShow'],
+  ];
+
+  const show = [
+    '#nbDesk', '#mail-icon', '#github-icon', '#about-link', '#projects-link', '#contact-link'
+  ];
+
+  window.onscroll = function () {
+    if (window.pageYOffset >= 1) {
+      for (let p = 0; p < shrinkThings.length; p++) {
+        $(shrinkThings[p][0]).addClass(shrinkThings[p][1]);
+      }
+      for (let q = 0; q < show.length; q++) {
+        $(show[q]).removeClass('noShow');
+      }
     }
-    for (let n = 0; n < navLink.length; n++) {
-      navLink[n].classList.remove('barlink-pin');
-    }
+  };
+
+  $('#headerTitle').click(function () {
     window.scrollTo(0, 0);
+    for (let r = 0; r < shrinkThings.length; r++) {
+      $(shrinkThings[r][0]).removeClass(shrinkThings[r][1]);
+    }
+    for (let s = 0; s < show.length; s++) {
+      $(show[s]).addClass('noShow');
+    }
+  });
+
+  $('#hamMenu').click(function () {
+    $('#hamMenu').toggleClass('is-active');
+    //$('#nbMobile').toggleClass('noShow');
   });
 });
 
