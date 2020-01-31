6a8b07170cec5dd2ea9815a8280d4d86c6842d34
Thomas Girgenti <thomasg@disroot.org>
2020-01-30 18:26:54 -0700

    update images with webp files

---
+++ scripts/img-grow.js
@@ -0,0 +1,43 @@
+let pcImgs;
+
+window.addEventListener("load", (event) => {
+
+    let noHover = window.matchMedia('(hover: none)').matches;
+
+    if (noHover) {
+        pcImgs = document.querySelectorAll(".pc-img");
+        createObserver();
+    }
+
+}, false);
+
+
+function createObserver() {
+    let observer;
+
+    let options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.75
+    };
+
+    observer = new IntersectionObserver(handleIntersect, options);
+
+    pcImgs.forEach(image => {
+        observer.observe(image);
+    });
+}
+
+
+function handleIntersect(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('pc-force-grow');
+        } else {
+            entry.target.classList.remove('pc-force-grow');
+        }
+    });
+}
+
+
+
